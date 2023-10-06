01c58854b1606493849bb14dd98d21bf679c8523
ozodbekmahammatjonov45 <147164836+ozodbekmahammatjonov45@users.noreply.github.com>
2023-10-06 18:37:34 +0500

    Add files via upload

---
+++ Reedwizer-shop/index.php
@@ -0,0 +1,503 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BOOVKMREM - shop</title>
+
+    <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+    
+<!-- header section starts  -->
+
+<header class="header">
+
+    <a href="./index.php" class="logo"> <img src="./image/retyme.svg" style="width: 50px;border-radius: 50%;align-items: center;"><a href="" style="font-size:3rem;color: #000;font-weight: 700;">ReTyme</a></a>
+
+    <nav class="navbar">
+        <a href="#home">home</a>
+        <a href="#features">features</a>
+        <a href="#products">products</a>
+        
+        <a href="#blogs">blogs</a>
+    </nav>
+
+    <div class="icons">
+        <div class="fas fa-bars" id="menu-btn"></div>
+        <div class="fas fa-search" id="search-btn"></div>
+        <div class="fas fa-shopping-cart" id="cart-btn"></div>
+        <div class="fas fa-user" id="login-btn"></div>
+    </div>
+
+    <form action="#" class="search-form">
+        <input type="search" id="search-box" placeholder="search here...">
+        <label for="search-box" class="fas fa-search"></label>
+    </form>
+<script type="text/javascript">
+    document.querySelector('.search-form #search-box').addEventListener('input', filterList);
+
+    function filterList(){
+        const searchInput = document.querySelector('.search-form #search-box');
+        const filter  =searchInput.value.toLowerCase();
+        const listItems = document.querySelectorAll('.list-group-item');
+
+        listItems.forEach((item)=>{
+            let text = item.textContent;
+            if(text.toLowerCase().includes(filter.toLowerCase())){
+                item.style.position = 'absolute';
+                item.style.top = '25%';
+                item.style.let = '30%';
+            }
+            else {
+                item.style.display = 'none';
+            }
+        })
+    }
+</script>
+    <div class="shopping-cart">
+        <div class="box">
+            <i class="fas fa-trash"></i>
+            <img src="image/phone1.png" alt="">
+            <div class="content">
+                <h3>Iphone</h3>
+                <span class="price">$700</span>
+                <span class="quantity">qty : 8</span>
+            </div>
+        </div>
+        <div class="box">
+            <i class="fas fa-trash"></i>
+            <img src="image/phone2.png" alt="">
+            <div class="content">
+                <h3>Samsung</h3>
+                <span class="price">$100/-</span>
+                <span class="quantity">qty : 5</span>
+            </div>
+        </div>
+        <div class="box">
+            <i class="fas fa-trash"></i>
+            <img src="image/pr2.png" alt="">
+            <div class="content">
+                <h3>space heater</h3>
+                <span class="price">$260</span>
+                <span class="quantity">qty : 11</span>
+            </div>
+        </div>
+        <div class="box">
+            <i class="fas fa-trash"></i>
+            <img src="image/pr4.png" alt="">
+            <div class="content">
+                <h3>Fridge</h3>
+                <span class="price">$500</span>
+                <span class="quantity">qty : 13</span>
+            </div>
+        </div>
+
+        <div class="total"> total : $1460/- </div>
+        <a href="#" class="btn">checkout</a>
+    </div>
+
+    <form action="" class="login-form">
+        <h3>login now</h3>
+        <input type="email" placeholder="your email" class="box">
+        <input type="password" placeholder="your password" class="box">
+        <p>forget your password <a href="#">click here</a></p>
+        <p>don't have an account <a href="#">create now</a></p>
+        <input type="submit" value="login now" class="btn">
+    </form>
+
+</header>
+
+<!-- header section ends -->
+
+<!-- home section starts  -->
+
+<section class="home" id="home">
+
+    <div class="content">
+        <h3>Our  <span>Electronic</span> products for you</h3>
+        <p>our products are very good and you can buy them too. Outside of the boom, you can find good things here, smart phones.</p>
+        <a href="#" class="btn">shop now</a>
+    </div>
+
+</section>
+
+<!-- home section ends -->
+
+<!-- features section starts  -->
+
+<section class="features" id="features">
+
+    <h1 class="heading"> our <span>products</span> </h1>
+
+    <div class="box-container">
+                <div class="box list-group-item">
+            <img src="image/phone1.png" alt="">
+            <h3>Iphone 11</h3>
+            <p>700$</p>
+            <a href="#" class="btn">Buy Now</a>
+        </div>
+        <div class="box list-group-item">
+            <img src="image/pr1.png" alt="">
+            <h3>washing machine</h3>
+            <p>400$</p>
+            <a href="#" class="btn">Buy Now</a>
+        </div>
+        <div class="box list-group-item">
+            <img src="image/pr10.png" alt="">
+            <h3>powerbank</h3>
+            <p>80$</p>
+            <a href="#" class="btn">Buy Now</a>
+        </div>
+         <div class="box list-group-item">
+            <img src="image/pr11.png" alt="">
+            <h3>Mouse</h3>
+            <p>8$</p>
+            <a href="#" class="btn">Buy Now</a>
+        </div>
+         <div class="box list-group-item">
+            <img src="image/pr2.png" alt="">
+            <h3>space heater</h3>
+            <p>260$</p>
+            <a href="#" class="btn">Buy Now</a>
+        </div>
+        <div class="box list-group-item">
+            <img src="image/pr3.png" alt="">
+            <h3>oven</h3>
+            <p>180$</p>
+            <a href="#" class="btn">Buy Now</a>
+        </div>
+         <div class="box list-group-item">
+            <img src="image/pr5.png" alt="">
+            <h3>hair</h3>
+            <p>70$</p>
+            <a href="#" class="btn">Buy Now</a>
+        </div>
+        <div class="box list-group-item">
+            <img src="image/pr6.png" alt="">
+            <h3>Fridge new</h3>
+            <p>350$</p>
+            <a href="#" class="btn">Buy Now</a>
+        </div>
+         <div class="box list-group-item">
+            <img src="image/pr7.png" alt="">
+            <h3>System Block</h3>
+            <p>152$</p>
+            <a href="#" class="btn">Buy Now</a>
+        </div>
+         <div class="box list-group-item">
+            <img src="image/pr8.png" alt="">
+            <h3>Television</h3>
+            <p>420$</p>
+            <a href="#" class="btn">Buy Now</a>
+        </div>
+         <div class="box list-group-item">
+            <img src="image/pr9.png" alt="">
+            <h3>Smart Watch</h3>
+            <p>121$</p>
+            <a href="#" class="btn">Buy Now</a>
+        </div>
+        <div class="box list-group-item">
+            <img src="image/feature-img-1.png" alt="">
+            <h3>fresh and organic</h3>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt, earum!</p>
+            <a href="#" class="btn">Buy Now</a>
+        </div>
+
+        <div class="box list-group-item">
+            <img src="image/feature-img-2.png" alt="">
+            <h3>free delivery</h3>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt, earum!</p>
+            <a href="#" class="btn">Buy Now</a>
+        </div>
+
+        <div class="box list-group-item">
+            <img src="image/feature-img-3.png" alt="">
+            <h3>easy payments</h3>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt, earum!</p>
+            <a href="#" class="btn">Buy Now</a>
+        </div>
+        <div class="box list-group-item">
+            <img src="image/product-1.png" alt="">
+            <h3>fresh orange</h3>
+            <p>$4.99/- - 10.99/-</p>
+            <a href="#" class="btn">read more</a>
+        </div>
+        <div class="box list-group-item">
+            <img src="image/product-3.png" alt="">
+            <h3>Meat</h3>
+            <p>$8.53/- - 15.24/-</p>
+            <a href="#" class="btn">Buy Now</a>
+        </div>
+        <div class="box list-group-item">
+            <img src="image/product-4.png" alt="">
+            <h3>cabbage</h3>
+            <p>$1.21/- - 5.10/-</p>
+            <a href="#" class="btn">Buy Now</a>
+        </div>
+        <div class="box list-group-item">
+            <img src="image/product-5.png" alt="">
+            <h3>Potato</h3>
+            <p>$8.53/- - 35.24/-</p>
+            <a href="#" class="btn">Buy Now</a>
+        </div>
+        <div class="box list-group-item">
+            <img src="image/product-6.png" alt="">
+            <h3>New Friut</h3>
+            <p>$8.53/- - 15.24/-</p>
+            <a href="#" class="btn">Buy Now</a>
+        </div>
+        <div class="box list-group-item">
+            <img src="image/product-7.png" alt="">
+            <h3>Carrot</h3>
+            <p>$3.41/- - 35.54/-</p>
+            <a href="#" class="btn">Buy Now</a>
+        </div>
+        <div class="box list-group-item">
+            <img src="image/product-8.png" alt="">
+            <h3>Green Lemon</h3>
+            <p>$1.36/- - 5.25/-</p>
+            <a href="#" class="btn">Buy Now</a>
+        </div>
+         <div class="box list-group-item">
+            <img src="image/cat-1.png" alt="">
+            <h3>Vegitables</h3>
+            <p>$9.00/- - 15.05/-</p>
+            <a href="#" class="btn">Buy Now</a>
+        </div>
+        <div class="box list-group-item">
+            <img src="image/cat-2.png" alt="">
+            <h3>Fresh fruits</h3>
+            <p>upto 45% off</p>
+            <a href="#" class="btn">Buy Now</a>
+        </div>
+        <div class="box list-group-item">
+            <img src="image/cat-3.png" alt="">
+            <h3>Dairy products</h3>
+            <p>upto 35% off</p>
+            <a href="#" class="btn">Buy Now</a>
+        </div>
+        <div class="box list-group-item">
+            <img src="image/a1.png" alt="">
+            <h3>Listener</h3>
+            <p>100$</p>
+            <a href="#" class="btn">Buy Now</a>
+        </div>
+        <div class="box list-group-item">
+            <img src="image/c1.png" alt="">
+            <h3>Computer Cor 7</h3>
+            <p>700$</p>
+            <a href="#" class="btn">Buy Now</a>
+        </div>
+          <div class="box list-group-item">
+            <img src="image/background.png" alt="">
+            <h3>all household appliances</h3>
+            <p>52000$</p>
+            <a href="#" class="btn">Applying</a>
+        </div>
+        <div class="box list-group-item">
+            <img src="image/c2.png" alt="">
+            <h3>electronic earpiece</h3>
+            <p>200$</p>
+            <a href="#" class="btn">Buy Now</a>
+        </div>
+        <div class="box list-group-item">
+            <img src="image/c3.png" alt="">
+            <h3>all household appliances</h3>
+            <p>4000$</p>
+            <a href="#" class="btn">Buy Now</a>
+        </div>
+        <div class="box list-group-item">
+            <img src="image/c4.png" alt="">
+            <h3>Keyboard, headset, mouse, gamepad</h3>
+            <p>1100$</p>
+            <a href="#" class="btn">Buy Now</a>
+        </div>
+        <div class="box list-group-item">
+            <img src="image/c5.png" alt="">
+            <h3>Gadgets</h3>
+            <p>500$</p>
+            <a href="#" class="btn">Buy Now</a>
+        </div>
+         <div class="box list-group-item">
+            <img src="image/fridge1.png" alt="">
+            <h3>Fridge</h3>
+            <p>500$</p>
+            <a href="#" class="btn">Buy Now</a>
+        </div>
+         <div class="box list-group-item">
+            <img src="image/iphone 13 pro.png" alt="">
+            <h3>Iphone 13 pro</h3>
+            <p>2000$</p>
+            <a href="#" class="btn">Buy Now</a>
+        </div>
+         <div class="box list-group-item">
+            <img src="image/laptop2.png" alt="">
+            <h3>Laptop</h3>
+            <p>900$</p>
+            <a href="#" class="btn">Buy Now</a>
+        </div>
+         <div class="box list-group-item">
+            <img src="image/pcm1.png" alt="">
+            <h3>desktop computer</h3>
+            <p>980$</p>
+            <a href="#" class="btn">Buy Now</a>
+        </div>
+        <div class="box list-group-item">
+            <img src="image/phone2.png" alt="">
+            <h3>Samsung</h3>
+            <p>20$</p>
+            <a href="#" class="btn">Buy Now</a>
+        </div>
+        <div class="box list-group-item">
+            <img src="image/t1.png" alt="">
+            <h3>Planshet</h3>
+            <p>100$</p>
+            <a href="#" class="btn">Buy Now</a>
+        </div>
+        <div class="box list-group-item">
+            <img src="image/w1.png" alt="">
+            <h3>Watch</h3>
+            <p>10$</p>
+            <a href="#" class="btn">Buy Now</a>
+        </div>
+    </div>
+
+</section>
+
+
+<!-- categories section ends -->
+
+<!-- blogs section starts  -->
+
+<section class="blogs list-group-item" id="blogs">
+
+    <h1 class="heading"> our <span>blogs</span> </h1>
+
+    <div class="box-container">
+
+        <div class="box list-group-item">
+            <img src="image/bg-dark.webp" alt="">
+            <div class="content">
+                <div class="icons">
+                    <a href="#"> <i class="fas fa-user"></i> by user </a>
+                    <a href="#"> <i class="fas fa-calendar"></i> 1st may, 2021 </a>
+                </div>
+                <h3>Computers iphone</h3>
+                <p>The case and wallet case are magnetically attached to the iPhone and to each other. </p>
+                <a href="#" class="btn">Buy Them</a>
+            </div>
+        </div>
+
+        <div class="box list-group-item">
+            <img src="image/little_gatget.jpg" alt="">
+            <div class="content">
+                <div class="icons">
+                    <a href="#"> <i class="fas fa-user"></i> by user </a>
+                    <a href="#"> <i class="fas fa-calendar"></i> 1st may, 2021 </a>
+                </div>
+                <h3>Little Gatgets</h3>
+                <p>Attach one AirTag to your keys and the other to your backpack.</p>
+                <a href="#" class="btn">Buy Them</a>
+            </div>
+        </div>
+
+        <div class="box list-group-item">
+            <img src="image/g.jpg" alt="">
+            <div class="content">
+                <div class="icons">
+                    <a href="#"> <i class="fas fa-user"></i> by user </a>
+                    <a href="#"> <i class="fas fa-calendar"></i> 1st may, 2021 </a>
+                </div>
+                <h3>Gatget's world</h3>
+                <p>See which stores of our official partners are closer to you.</p>
+                <a href="#" class="btn">Buy Them</a>
+            </div>
+        </div>
+
+    </div>
+
+</section>
+
+<!-- blogs section ends -->
+
+<!-- footer section starts  -->
+
+<section class="footer">
+
+    <div class="box-container">
+
+        <div class="box">
+            <h3> ReTyme <img src="../image/retyme.svg" style="width:50px;border-radius:50%;align-items: center;margin-bottom: -20px;"> </h3>
+            <p>Reedwizer is a bid company and there are online shop and app</p>
+            <div class="share">
+                <a href="#" class="fab fa-facebook-f"></a>
+                <a href="#" class="fab fa-twitter"></a>
+                <a href="#" class="fab fa-instagram"></a>
+                <a href="#" class="fab fa-linkedin"></a>
+            </div>
+        </div>
+
+        <div class="box">
+            <h3>contact info</h3>
+            <a href="#" class="links"> <i class="fas fa-phone"></i> +998913484715 </a>
+            <a href="#" class="links"> <i class="fas fa-phone"></i> +998912932915 </a>
+            <a href="#" class="links"> <i class="fas fa-envelope"></i> retyme@gmail.com </a>
+            <a href="#" class="links"> <i class="fas fa-map-marker-alt"></i> Namangan, Uzbekistan </a>
+        </div>
+
+        <div class="box">
+            <h3>quick links</h3>
+            <a href="#" class="links"> <i class="fas fa-arrow-right"></i> home </a>
+            <a href="#" class="links"> <i class="fas fa-arrow-right"></i> features </a>
+            <a href="#" class="links"> <i class="fas fa-arrow-right"></i> products </a>
+            <a href="#" class="links"> <i class="fas fa-arrow-right"></i> categories </a>
+            <a href="#" class="links"> <i class="fas fa-arrow-right"></i> review </a>
+            <a href="#" class="links"> <i class="fas fa-arrow-right"></i> blogs </a>
+        </div>
+
+        <div class="box">
+            <h3>newsletter</h3>
+            <p>subscribe for latest updates</p>
+            <input type="email" placeholder="your email" class="email">
+            <input type="submit" value="subscribe" class="btn">
+            <img src="image/payment.png" class="payment-img" alt="">
+        </div>
+
+    </div>
+
+    <div class="credit"> Company <span> ReTyme online shop </span> | all rights reserved </div>
+
+</section>
+
+<!-- footer section ends -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
